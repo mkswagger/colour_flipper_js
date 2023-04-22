ec2c48e41077455579b84d1f0234576d449e20b5
mkswagger <mathangykrishna16@gmail.com>
2023-04-23 02:35:37 +0530

    colorflipper

--- color_flipper/app.js
+++ color_flipper/app.js
@@ -3,5 +3,12 @@ const btn=document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click",function(){
-    
-})
+    const randomNumber=getRandomNumber();//rand no between 0 and 3
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent=colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
